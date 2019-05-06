81509dd4c4803359e9ecf67112b01f36974a8054
takeshi0406 <sci.and.eng@gmail.com>
2019-05-06 12:59:13 +0900

    tmp

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const request = require('request');
 const app = express();
 
 app.use(async (req, res) => {
-  const url = req.query.url;
-
-  if (!url) {
-    return res.send('Please provide URL as GET parameter, for example: <a href="/?url=https://example.com">?url=https://example.com</a>');
-  }
-
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox']
-  });
-  const page = await browser.newPage();
-  await page.goto(url);
-  const imageBuffer = await page.screenshot();
-  browser.close();
-
-  res.set('Content-Type', 'image/png');
-  res.send(imageBuffer);
+  postSlack("test");
+  res.send("test");
 });
 
 const server = app.listen(process.env.PORT || 8080, err => {
@@ -26,3 +13,23 @@ const server = app.listen(process.env.PORT || 8080, err => {
   const port = server.address().port;
   console.info(`App listening on port ${port}`);
 });
+
+
+const postSlack = (message) => {
+  const request = require('request');
+  const options = {
+    uri: process.env.SLACK_ENDPOINT,
+    headers: {
+      "Content-type": "application/json",
+      },
+      json: {
+        "text": message,
+        "unfurl_links": true
+      }
+    };
+  request.post(options, (error, response, body) => {
+    console.log(error);
+    console.log(response);
+    console.log(body);
+  });
+}
