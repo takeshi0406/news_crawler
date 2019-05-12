2d2fe156d88e7dfe8ac87af7d24ad84abb23e004
takeshi0406 <sci.and.eng@gmail.com>
2019-05-12 13:23:41 +0900

    tmp

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const TwitterClient = require('./lib/twitter')
 const app = express();
 require('dotenv').config();
 
-const twclient = new TwitterClient(
-  process.env.TWITTER_CONSUMER_KEY,
-  process.env.TWITTER_CONSUMER_SECRET,
-  process.env.TWITTER_TOKEN,
-  process.env.TWITTER_TOKEN_SECRET
-)
-console.log(twclient.requestUrls("takeshi0406", "fudosan", 1));
+
+const main = () => {
+  const twclient = new TwitterClient(
+    process.env.TWITTER_CONSUMER_KEY,
+    process.env.TWITTER_CONSUMER_SECRET,
+    process.env.TWITTER_TOKEN,
+    process.env.TWITTER_TOKEN_SECRET
+  )
+  console.log(twclient.getNewsUrls("takeshi0406", "fudosan", 100));
+}
 
 
+main();
+
+/*
 app.use(async (req, res) => {
   const client = new ChatWorkClient(process.env.CHATWORK_TOKEN);
   client.postMessage("31958529", "test");
@@ -25,3 +31,4 @@ const server = app.listen(process.env.PORT || 8080, err => {
   const port = server.address().port;
   console.info(`App listening on port ${port}`);
 });
+*/
