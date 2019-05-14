7a1a8577c0097d95324c928c6a9375b3a363693e
takeshi0406 <sci.and.eng@gmail.com>
2019-05-14 22:39:47 +0900

    result

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const ChatWorkRoomManager = require('./lib/chatwork');
 const TwitterClient = require('./lib/twitter')
-const UrlUtils = require('./lib/urlutils')
 const Crawler = require('./lib/crawler')
 
 require('dotenv').config();
 
 
 const main = () => {
-    crawl().catch((error) => {
+    async_main().catch((error) => {
         throw error;
     })
 }
 
-const crawl = async () => {
+const async_main = async () => {
     const twclient = new TwitterClient(
         process.env.TWITTER_CONSUMER_KEY,
         process.env.TWITTER_CONSUMER_SECRET,
@@ -22,14 +21,36 @@ const crawl = async () => {
     const cwclient = new ChatWorkRoomManager(process.env.CHATWORK_TOKEN, 31958529);
 
     const known_urls = await cwclient.getPostedUrls();
-    const news = await twclient.getNews("takeshi0406", "fudosan", 20);
-    const news_urls = news.filter((x) => {
+    const news = (await twclient.getNews("takeshi0406", "fudosan", 20)).filter((x) => {
         return !known_urls.has(x.url);
-    }).map(x => x.url);
-    const results = await Crawler.crawlAllPages(Array.from(news_urls));
+    });
+    const results = await crawl(news);
+    
     console.log(results.filter((res) => {
-        return !known_urls.has(res.redirected_url);
+        return !known_urls.has(res.page.redirected_url);
     }));
+}
+
+
+const crawl = async (news) => {
+    const pages = await Crawler.crawlAllPages(news.map(x => x.url));
+    const results = news.map((x, i) => {
+        return new Result(x, pages[i]);
+    });
+    const grouped = results.reduce((acc, x) => {
+        const y = acc.get(x.page.redirected_url);
+        if (!y || x.news.popularity > y.news.popularity)
+            acc.set(x.page.redirected_url, x);
+        return acc;
+    }, new Map());
+    return Array.from(grouped.values());
+}
+
+class Result {
+    constructor(news, page) {
+        this.news = news;
+        this.page = page;
+    }
 }
 
 main();
