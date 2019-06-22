26de9d51371374b039b20a853318abbd5a0bc409
takeshi0406 <sci.and.eng@gmail.com>
2019-06-22 10:50:28 +0900

    add test case

--- spec/lib/crawler.spec.js
+++ spec/lib/crawler.spec.js
@@ -5,11 +5,18 @@ const crawler = rewire('../../lib/crawler');
 describe("", () => {
     describe("", () => {
         fetchPage = crawler.__get__("fetchPage");
-        it("", (done) => {
+        it("UTF-8のページのとき", (done) => {
             fetchPage("https://kiito.hatenablog.com/").then(response => {
                 expect(response.title).toEqual("歩いたら休め");
                 done();
-            })
+            });
+        });
+
+        it("", (done) => {
+            fetchPage("https://monoist.atmarkit.co.jp/mn/articles/1806/12/news057.html").then(response => {
+                expect(response.title).toEqual("日立は「カンパニー」から「ビジネスユニット」へ、成長のエンジンは「Lumada」 - MONOist（モノイスト）");
+                done();
+            });
         })
     })
 })
