6356d46de02acc5c2bde0ca2a58a8288d4026450
takeshi0406 <sci.and.eng@gmail.com>
2019-07-11 08:34:23 +0900

    tmp

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -9,8 +9,8 @@ describe("", () => {
             "title": "NewsCrawlerのテスト",
             "twlist": "takeshi0406/fintech",
             "chatroom": 31958529,
-            "hatenaId":" takeshi0406",
-            "blogId":"finnews.hatenablog.com"
+            "hatenaId": "takeshi0406",
+            "blogId": "finnews.hatenablog.com"
         }));
         index.executeNewsCrawler(event, null);
     }, 10 * 60 * 1000);
