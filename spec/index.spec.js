57a00ddbceba78289a99e18447a46dd53d623c24
takeshi0406 <sci.and.eng@gmail.com>
2019-07-10 22:11:53 +0900

    tmp

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,13 +3,15 @@ const index = rewire('../index');
 
 
 describe("", () => {
-    const event = {};
-    event.data = new Buffer(JSON.stringify({
-        "title": "NewsCrawlerのテスト",
-        "twlist": "takeshi0406/fintech",
-        "chatroom": 31958529,
-        "hatenaId":" takeshi0406",
-        "blogId":"finnews.hatenablog.com"
-    }));
-    index.executeNewsCrawler(event, null);
+    it("", (done) => {
+        const event = {};
+        event.data = new Buffer(JSON.stringify({
+            "title": "NewsCrawlerのテスト",
+            "twlist": "takeshi0406/fintech",
+            "chatroom": 31958529,
+            "hatenaId":" takeshi0406",
+            "blogId":"finnews.hatenablog.com"
+        }));
+        index.executeNewsCrawler(event, null);
+    });
 });
